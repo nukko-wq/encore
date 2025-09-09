c919da73102acf8b07b745cf5a0c3aebb88b7bbb
nukko <shouta.tsuboi@gmail.com>
2025-09-09 10:27:08 +0900

    検索エンジンにインデックスされないように変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,19 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'Encore - ブックマーク管理システム',
   description: '高機能なブックマーク管理システム',
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 }
 
 export default function RootLayout({
